ad37aa6eae6e7b498b2971e7d3ea4ae6bed76868
Alexa <48775628+alexalpz@users.noreply.github.com>
2020-02-15 03:37:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,46 +6,47 @@ sudo chmod -R g+w /var/www/html/uploads
 sudo chmod -R g+w /var/www/html/thumb
 */
 
-//
+//The first line tells PHP you'd like to see any errors that occur [1]. The second line will determine if the errors will be displayed or hidden to the user. The third line "error_reporting" sets which PHP errors are reported[2]. "E_ALL" reports all PHP errors[3].
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//
+//Setting "dbconnecterror" variable to “false” won't display anything.  "dbh" is placeholder variable that will connect to the webserver. 
 $dbconnecterror = FALSE;
 $dbh = NULL;
 
-//
+//Two variables with empty strings. 
 $username = "";
 $password = "";
 
-//
+//This variable is attempting to create an array but has nothing in it's parameters.
 $errors = array();
 
-//
+//This determines whether the request was a POST or GET request, in this case it is GET [4]
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	
-	//
+	//The "array_key_exists" checks if the given key or index exists in the array. The "error" string specifies the key, and the $_GET specifies which array [5]. setcookie() defines a cookie to be sent along with the rest of the HTTP headers [6]. Sets the experation date to one hour ago. 
 	if(array_key_exists("logout", $_GET)){
 		setcookie("wishit_session_id", "", time() - 3600);
 		setcookie("wishit_isadmin", "", time() - 3600);
 	}
 }
 
-//
+//This determines whether the request was a POST or GET request, in this case it is POST [4]
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-	// 
+	//Variables passed to the current script via  HTTP post method [7]. The script is require user input to store the following information into database.
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
 	try{
-		//
+		//Blocks of code attempting to connect to the web server and database. First line is connecting to web server and database and the second line is connecting as the admin using the password required to be authorized.  PDO ERRMODR attribute (controls error reporting) to raise a PDOException that tells you what went wrong when it goes wrong. [8].
 		$conn_string = "mysql:host=localhost;dbname=wishit";
-		$dbh= new PDO($conn_string, "root", "password");
+		$dbh= new PDO($conn_string, "phpmyadmin", "studentstudent");
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		//
+		//"$sql" variable storing query listing all information of user with set username and password as a search method in database.  The "prepare" statement is sent to the database server.During execute the client binds parameter values and sends them to the server. Fetch all of the remaining rows in the result set from database [9].
+	//TODO: Execution line should have it's own variable to call. 
 		$sql = "SELECT * FROM users where username = '$username' AND password = '$password';";
 		$stmt = $dbh->prepare($sql);
 		$stmt->execute();
@@ -149,6 +150,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 				<?php } ?>
 				<ul>
 			</div>
+<?php 
+/*Sources:
+[1]  (http://www.peachpit.com/articles/article.aspx?p=674688&seqNum=4)
+[2]  (https://www.php.net/manual/en/function.error-reporting.php)
+[3]  (https://www.php.net/manual/en/function.error-reporting.php)
+[4]  (https://www.quora.com/Why-do-some-PHP-programmers-use-_SERVER-REQUEST_METHOD-POST)
+[5]  (https://www.w3schools.com/php/func_array_key_exists.asp)
+[6]  (https://www.php.net/manual/en/function.setcookie.php)
+[7]  (https://www.php.net/manual/en/reserved.variables.post.php)
+[8]  (https://www.quora.com/What-is-SetAttribute-PDO-ATTR_ERRMODE-PDO-ERRMODE_EXCEPTION-in-PHP)
+[9]  (https://www.php.net/manual/en/pdostatement.fetchall.php)
+					
+*/
+
+?>
 		<?php } ?>
 		
 		
