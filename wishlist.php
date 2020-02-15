85bc1b1c479b1cd999858bd57373b52b5b557cc6
Alexa <48775628+alexalpz@users.noreply.github.com>
2020-02-15 15:36:14 -0500

    Update wishlist.php

--- wishlist.php
+++ wishlist.php
@@ -1,53 +1,58 @@
 <?php
 
-//
+//The isset() function return false if testing variable contains a NULL value [1]. The value in this case contains the value of a cookie [2].
 if (isset($_COOKIE['wishit_session_id'])) {
 
-	//
+	//The $email variable stores built in function "Base64" to grab the encoded cookie value, if it exists, and returns the decoded data, if it has been encoded [3]. 
 	$email = base64_decode($_COOKIE['wishit_session_id']);
 
 } else {
 
-	//
+	////This attempts to store a HTTP header of a unlogged page due to missing cookie value but redirects do no work due to PHP variables, in this case "?notloggedin=true" [4]. The "exit" function is missing a required message to print before terminating the script [5].
+	//TODO: Add some form of exit message to "exit" function. 
 	header("Location: index.php?notloggedin=true");
 	exit();
 	
 }
 
-//
+//The "Try" statment is attempting to connect to the web server but it may throw an exception if the following block of code does not go through succesfully [6]. 
 try{
 
-	//
+	//Blocks of code attempting to connect to the web server and database. First line is connecting to web server and database and the second line is connecting as the admin using the password required to be authorized.  PDO ERRMODR attribute (controls error reporting) to raise a PDOException that tells you what went wrong when it goes wrong. [7]
 	$conn_string = "mysql:host=localhost;dbname=wishit";
-	$dbh= new PDO($conn_string, "root", "password");
+	$dbh= new PDO($conn_string, "phpmyadmin", "studentstudent");
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	//
+	//$sql performs a query, selecting all information of user with the specific email address.  The "prepare" statement is sent to the database server. During execute the client binds parameter values and sends them to the server
+	//TODO: Add variable name to execution line. 
 	$sql = "SELECT * FROM users where email = '$email';";
 	$stmt = $dbh->prepare($sql);
 	$stmt->execute();
 
-	//
+	//Fetch all of the remaining rows in the result set from database [8].
+	//TODO: Call proper variable name once named the execution line above. 
 	$records = $stmt->fetchAll();
 
-	// 
+	// If the data does not exists, continue with the following blocks of code.
 	if (count($records) < 1) {
 
-		//
+		// Continue to send a HTTP header to the server display error on login page.  \
+		//TODO: Add required message to exit parameter. 
 		header("Location: index.php?notloggedin=true");
 		exit();
 
 	}
 
-	//
+	//$sql variable querie that lists out all info from a "wishes" table identified by specific email address. no variable is storing the execution.
+	//TODO: Create variable name for execution. 
 	$sql = "SELECT * FROM wishes where email = '$email';";
 	$stmt = $dbh->prepare($sql);
 	$stmt->execute();
 
-	//
+	//The $user variable returns an array containing all of the remaining rows in the result set [9]. 
 	$wishes = $stmt->fetchAll();
 
-//
+//This catch stament is attempting to catch botch exeptions and errors by adding a catch block for exception after catching the throwable first "$e" [10] . It stores error message in variable and the error handler gets called but knows nothing about $dbh->errorCode.
 }catch(Exception $e){
 	$errors[] = "There was an error connecting to the database. Please try again later. Error code " . $dbh->errorCode();
 }
@@ -64,7 +69,8 @@ try{
 
 	<body>
 		
-		<!-- 			 -->
+		<!--  Will check if the header.php file has already been included, and if so, not include (require) it again. -->
+		<!--TODO: Will fix the position of table tags and correctly format it in order -->
 		<?php require_once('include/header.php');?>
 		<table>
 			<tr>				
@@ -72,12 +78,14 @@ try{
 			</tr>			
 	
 			<?php 
-			//
+			//If no rows are found in the wishes table, the code intends to display an error message
+			//TODO: Error message is not outputting correctly. Should fix this. 
 			if (sizeof($wishes) == 0) { ?>
 				No wish list found.
 			
 			<?php 
-			//
+			//If rows of data are found, continue with the following lines of code. 
+				//TODO: Add a table tag to the table headers. Add <td> tags for data. 
 			} else { 	
 			?>
 			<tr>
@@ -88,7 +96,8 @@ try{
 
 			<tr>
 				<?php 
-					//
+					// foreach ($wishes as $wish) creates just one database query[11]. It intends to store wishid, description, url and image file. 
+					// TODO: Remove seperating PHP and just join them together with the lines of code it hopes to execute. Do not open in one and continue with closing tag in another PHP. Bad practice. 
 					foreach ($wishes as $wish) { ?>
 					<?php 
 
@@ -167,6 +176,25 @@ try{
 						for($i = 0; $i < count($errors); $i++) {
 							echo $errors[$i];
 						} ?>
+					
+					
+<?php /*
+Sources: 
+[1] https://www.w3resource.com/php/function-reference/isset.php
+[2] https://www.php.net/manual/en/function.setcookie.php
+[3] https://www.base64decoder.io/php/
+[4] https://my.bluehost.com/hosting/help/241
+[5] https://www.w3schools.com/php/func_misc_exit.asp
+[6] https://stackify.com/php-try-catch-php-exception-tutorial/
+[7] https://www.quora.com/What-is-SetAttribute-PDO-ATTR_ERRMODE-PDO-ERRMODE_EXCEPTION-in-PHP
+[8] https://www.php.net/manual/en/pdostatement.fetchall.php
+[9] https://www.php.net/manual/en/pdostatement.fetchall.php\
+[10]https://www.php.net/manual/en/language.errors.php7.php
+[11]https://stackoverflow.com/questions/43322413/laravel-php-foreachuserall-as-user-performance
+					
+					
+					
+*/ ?>
 				</div>
 			<?php } ?>
 		<hr>
