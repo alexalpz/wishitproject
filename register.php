dc4f0f9e5793d45a7023bb8afb316f6c152aba38
Alexa <48775628+alexalpz@users.noreply.github.com>
2020-02-15 04:35:31 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,14 +1,14 @@
 <?php
-//
+//The first line tells PHP you'd like to see any errors that occur [1]. The second line will determine if the errors will be displayed or hidden to the user. The third line "error_reporting" sets which PHP errors are reported[2]. "E_ALL" reports all PHP errors[3].
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//
+//Setting "dbconnecterror" variable to “false” won't display anything.  "dbh" is placeholder variable that will connect to the webserver.
 $dbconnecterror = FALSE;
 $dbh = NULL;
 
-//
+//Variables with empty strings. No way of inputting through this unless modified and updated later.
 $email = "";
 $username = "";
 $password = "";
@@ -16,46 +16,48 @@ $securityquestion = "";
 $securityanswer = "";
 
 
-//
+//Variable attempts to create an array to whatever is in parameter. Nothing is in the parameter currently. 
 $errors = array();
 
-//
+//This determines whether the request was a POST or GET request, in this case it is POST.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-	//
+	//The trim() removes whitespaces and also the predefined characters from both sides of a string that is left and right [4]. $_POST is also widely used to pass variables [5]
 	$email = trim($_POST['email']);
 	$username = trim($_POST['username']);
 	$password = trim($_POST['password']);
 	$securityquestion = trim($_POST['securityquestion']);
 	$securityanswer = trim($_POST['securityanswer']);
 
-	//
+	//All of the code within the try block is executed until an exception is potentially thrown. 
 	try{
 
-		//
+		//First line is connecting to web server and database and the second line is connecting as the admin using the password required to be authorized.  PDO ERRMODE attribute (controls error reporting) to raise a PDOException that tells you what went wrong when it goes wrong. [6]
 		$conn_string = "mysql:host=localhost;dbname=wishit";
-		$dbh= new PDO($conn_string, "root", "password");
+		$dbh= new PDO($conn_string, "phpmyadmin", "studentstudent");
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		//
+		//$sql variable concatenates two query strings. The "prepare" statement is sent to the database server[7].During execute the client binds parameter values and sends them to the server[7].
+				//TODO: Join querie strings together. Remove quotation marks on variables. Would add only a single querie in "prepare" parameters.
 		$sql = "INSERT INTO users (email, username, password, securityquestion, securityanswer) " .
 			"VALUES ('$email', '$username', '$password', '$securityquestion', '$securityanswer')";			
 		$stmt = $dbh->prepare($sql);
 		$success = $stmt->execute();
 
-		// 
+		// If the query above is executed succesfully, run this "IF..." statement. 
 		if ($success) {
 
-			//
+			// A header intended to display a user has registered as a HTTP header. Redirect brower to said page. 
+			//TODO: Add exist message to exit parameter.
 			header("Location: index.php?registered=true");
 			exit();
 			
-		//
+		//Store a error message concatenated with errorCode function. The error handler gets called but knows nothing about $dbh->errorCode. [8]
 		} else {
 			$errors[] = "There was a problem completing your registration. Error code " . $dbh->errorCode();
 		}
 
-	//
+	//This catch stament is attempting to catch botch exeptions and errors by adding a catch block for exception after catching throwable first, "$e" [9] . The error handler gets called but knows nothing about $dbh->errorCode. [8].
 	}catch(Exception $e){
 		$errors[] = "There was an error connecting to the database. Please try again later. Error code " . $dbh->errorCode();
 	}
@@ -151,7 +153,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				}				  
 			  
 			}
+			<?php
+			/*Sources:
+			[1]  (http://www.peachpit.com/articles/article.aspx?p=674688&seqNum=4)
+			[2]  (https://www.php.net/manual/en/function.error-reporting.php)
+			[3]  (https://www.php.net/manual/en/function.error-reporting.php)
+			[4]  (https://www.geeksforgeeks.org/php-trim-function/)
+			[5]  (https://www.w3schools.com/php/php_superglobals_post.asp)
+			[6]  (https://www.quora.com/What-is-SetAttribute-PDO-ATTR_ERRMODE-PDO-ERRMODE_EXCEPTION-in-PHP)
+			[7]  (https://www.geeksforgeeks.org/why-to-check-both-isset-and-empty-function-in-php/)
+			[8]  (https://www.php.net/manual/en/pdostatement.errorcode.php)
+			[9]  (https://www.php.net/manual/en/language.errors.php7.php)
 			
+			
+			
+			
+			*/
+			?>
 			
 		</script>
 		
